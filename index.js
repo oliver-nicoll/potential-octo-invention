87a9b5d2752766f4a4a71a767643d87f540aba75
Nicoll Oliver <nicolldon96@gmail.com>
2022-10-01 12:05:48 -0400

    final

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //server
-const PORT = 8000
+const PORT = process.env.PORT || 8000
+
 const express = require('express')
+
 const axios = require('axios')
+
 const cheerio = require('cheerio')
 
 const newspapers = [
@@ -19,6 +22,31 @@ const newspapers = [
         name: 'telegraph',
         address: 'https://www.telegraph.co.uk/climate-change',
         base: 'https://www.telegraph.co.uk'
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk'
+    },
+    {
+        name: 'es',
+        address: 'https://www.standard.co.uk/topic/climate-change',
+        base: 'https://www.standard.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/topic/climate-change-environment',
+        base: ''
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base: ''
+    },
+    {
+        name: 'nyp',
+        address: 'https://nypost.com/tag/climate-change/',
+        base: ''
     }
 ]
 
@@ -44,12 +72,39 @@ newspapers.forEach(newspaper => {
             })
         })
 })
+
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change API')
 })
 
 app.get('/news', (req, res) => {
     res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+    
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                console.log(url)
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
 })
 
 
